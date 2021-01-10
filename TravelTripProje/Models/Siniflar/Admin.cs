9f4f1ef3a1fda0e1223adc24f13dfdd80d3985ba
fatihakd <fatihakd0323@gmail.com>
2021-01-10 20:11:36 +0300

    TravelTripProje

---
+++ TravelTripProje/Models/Siniflar/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class Admin
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Kullanici { get; set; }
+        public string Sifre { get; set; }
+    }
+}
